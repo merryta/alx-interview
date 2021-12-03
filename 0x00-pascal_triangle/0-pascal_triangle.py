57748f9da462b35b0fd743756ccc3f135e03ef89
merryta <merryta251@gmail.com>
2021-12-03 01:37:33 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+founction returns a list of lists of integers repesent \
+the pascal's triangle of n
+"""
+def pascal_triangle(n):
+    x=[[]for i in range(n)]
+    for i in range(n):
+        for j in range(i+1):
+            if (j<i):
+                if(j==0):
+                    x[i].append(1)
+                else:
+                    x[i].append(x[i-1][j]+x[i-1][j-1])
+            elif(j==i):
+                x[i].append(1)
+    return x
