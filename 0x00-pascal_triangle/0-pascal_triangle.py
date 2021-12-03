095baa9e4d97dc7bcfeacc376ba5cdd096776d63
merryta <merryta251@gmail.com>
2021-12-03 03:19:32 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,18 +3,20 @@
 founction returns a list of lists of integers repesent \
 the pascal's triangle of n
 """
+
+
 def pascal_triangle(n):
     """
     pascal triangle that out put with any value
     """
-    x=[[]for i in range(n)]
+    x = [[]for i in range(n)]
     for i in range(n):
         for j in range(i+1):
-            if (j<i):
-                if(j==0):
+            if (j < i):
+                if (j == 0):
                     x[i].append(1)
                 else:
                     x[i].append(x[i-1][j]+x[i-1][j-1])
-            elif(j==i):
+            elif (j == i):
                 x[i].append(1)
     return x
